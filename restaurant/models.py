11e43d6a8eaeb1119a81032ad1a11c7c8896a20f
ikswary <ikswary@naver.com>
2020-08-11 15:50:55 +0900

    add : restaurant, menu model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    address = models.CharField(max_length=200, null=False)
+    phone_number = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=500, null=False)
+
+    class Meta:
+        db_table = 'restaurants'
+
+
+class Menu(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT, null=False)
+    name = models.CharField(max_length=50, null=False)
+    price = models.IntegerField()
+
+    class Meta:
+        db_table = 'menus'
